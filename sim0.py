3a43148229cff2207d833881cc6e711289e50722
arash <kooroshnezhad2004@gmail.com>
2023-02-07 18:29:52 +0330

    added four doors

--- sim0.py
+++ sim0.py
@@ -1,22 +1,28 @@
 import random
 # Now let’s do that 100000 times.
 # Make 10000 trials.
-n_tries = 100
+n_tries = 10000000
 # Lists to store results from stay and switch strategy
 stay_results = []
-switch_results = []
+switch_results1 = []
+switch_results2 = []
 for i in range(n_tries):
     # Same code as above, for one trial
-    doors = ['car', 'goat', 'goat']
+    doors = ['car', 'goat', 'goat', 'goat']
     random.shuffle(doors)
-    my_door_index = random.choice([0, 1, 2])
+    my_door_index = random.choice([0, 1, 2, 3])
     stay_result = doors.pop(my_door_index)
     doors.remove('goat')
-    switch_result = doors[0]
+    switch_result1 = doors[0]
+    switch_result2 = doors[1]
     # Put results into result lists
     stay_results.append(stay_result)
-    switch_results.append(switch_result)
+    switch_results1.append(switch_result1)
+    switch_results2.append(switch_result2)
 
-stay_results.count('car') / n_tries
 
-switch_results.count('car') / n_tries
+print(stay_results.count('car') / n_tries)
+
+print(switch_results1.count('car') / n_tries)
+
+print(switch_results2.count('car') / n_tries)
